64711418e6017464b0fb45dd78a5cc30a24327f1
rahul7900 <rahul.singh7920000@gmail.com>
2021-07-30 18:25:54 +0530

    This is to do list

---
+++ main.js
@@ -0,0 +1,59 @@
+const addButton = document.querySelector('#add');
+const updateLSDAta = () => {
+    const textAreaData = document.querySelectorAll('textarea');
+    const notes = [];
+    textAreaData.forEach((note) => {
+        return notes.push(note.value);
+
+
+    })
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+const addNewNote = (text = '') => {
+    const note = document.createElement('div');
+    note.classList.add('note');
+    const htmlData = ` 
+     <div class="operation">
+    <button class="edit"> <i class="fas fa-edit"></i></button>
+    <button class="delete"><i class="fas fa-trash-alt"></i></button>
+</div>
+<div class="main  ${text ? "" : "hidden"}"></div>
+        <textarea class="${text ? "hidden" : ""}"></textarea> `;
+
+    note.insertAdjacentHTML('afterbegin', htmlData)
+
+    document.body.appendChild(note)
+    // getting the references
+    const editButton = note.querySelector('.edit');
+    const delButton = note.querySelector('.delete');
+    const mainDiv = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+    //deleting the node
+    delButton.addEventListener('click', () => {
+        note.remove();
+        updateLSDAta()
+    })
+    // toggle using edit button
+    textArea.value = text;
+    mainDiv.innerHTML = text;
+
+
+    editButton.addEventListener('click', () => {
+        mainDiv.classList.toggle('hidden');
+        textArea.classList.toggle('hidden');
+    })
+    textArea.addEventListener('change', (event) => {
+        const value = event.target.value;
+        mainDiv.innerHTML = value;
+        updateLSDAta();
+    })
+}
+// getting data back from localStorage
+const notes = JSON.parse(localStorage.getItem('notes'));
+if (notes) {
+    notes.forEach((note) => addNewNote())
+}
+
+addButton.addEventListener('click', () => {
+    addNewNote()
+})
